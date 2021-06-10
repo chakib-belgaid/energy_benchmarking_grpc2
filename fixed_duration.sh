2f47dd2588079be6f49a12d022e9b8da2fd1b4f6
chakib-belgaid <chakib.belgaid@gmail.com>
2021-06-10 10:33:28 +0200

    update : client

--- fixed_duration.sh
+++ fixed_duration.sh
@@ -3,4 +3,6 @@
 export GRPC_MODE="fix"
 export GRPC_BENCHMARK_STOP_CRETERION="duration"
 export GRPC_BENCHMARK_DURATION="5s"
+export GRPC_REQUEST_PAYLOAD="100KB"
+export HWPC_DURATION=100
 ./benchmarkit.sh $@
